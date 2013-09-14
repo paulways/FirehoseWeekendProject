8e428069304cc7f75e2ee0c590ee3ee2c90014d2
Paul Preston <paul@Claytons-MacBook-Pro.local>
2013-09-14 16:13:10 -0400

    First commit

---
+++ app/controllers/staticpages_controller.rb
@@ -0,0 +1,8 @@
+class StaticpagesController < ApplicationController
+
+	def index
+	end
+
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firehose3::Application.config.secret_token = '60a8fc81e6cabad3abe52f4f80a0a9456d35cbd915a0585aabb8e6fabbb58bc1c9cd0fd005ba49d301c2f32f251601471b4dda77a6d75746c697f6bab220086d'
